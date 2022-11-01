f27b1e0b40ee8be99c99864e45024400ed773317
Kato Hiro <70745669+hirozo99@users.noreply.github.com>
2022-11-01 12:37:07 +0900

    Create 014.py

---
+++ math-and-algorithm/014.py
@@ -0,0 +1,17 @@
+#自然数Nを素因数分解するプログラムを作成してください。
+
+N = int(input())
+
+# 素因数分解
+Answer = []
+LIMIT = int(N ** 0.5)
+for i in range(2, LIMIT + 1):
+	while N % i == 0:
+		N /= i
+		Answer.append(i)
+
+if N >= 2:
+	Answer.append(int(N))
+
+# 出力
+print(*Answer)
