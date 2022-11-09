144b4b544d5dc1f5ec43b32dd9ef378a3039be23
Kato Hiro <70745669+hirozo99@users.noreply.github.com>
2022-11-09 22:01:46 +0900

    Update 025.py

--- math-and-algorithm/025.py
+++ math-and-algorithm/025.py
@@ -3,8 +3,10 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+#答えを求める
 ans = 0
 for i in range(N):
 	ans += (1 / 3) * A[i] + (2 / 3) * B[i]
 
-print(ans)
+#出力
+print("%.7f" % ans)
