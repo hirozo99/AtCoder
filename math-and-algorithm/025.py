75089ba0efbf68ad870a9f53e3ea68ea2562dc40
Kato Hiro <70745669+hirozo99@users.noreply.github.com>
2022-11-09 21:58:09 +0900

    Create 025.py

---
+++ math-and-algorithm/025.py
@@ -0,0 +1,10 @@
+#入力
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = 0
+for i in range(N):
+	ans += (1 / 3) * A[i] + (2 / 3) * B[i]
+
+print(ans)
