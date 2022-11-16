4b7a75b3a4542984de57822fcf9910de54ad6da4
hirozo99 <hiro.1999@me.com>
2022-11-16 19:59:06 +0900

    add

--- ABC277/B.py
+++ ABC277/B.py
@@ -1,18 +1,3 @@
-# N = int(input())
-# S = [input() for _ in range(N)]
-#
-# def is_ans():
-#     for i in range(N):
-#         if S[i].find("H" or "D" or "C" or "S") == 0:
-#             if S[i].find("A" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "T" or "J" or "Q" or "K" ) == 1:
-#                 return True
-#         return False
-#
-# if is_ans():
-#     print("Yes")
-# elif not is_ans():
-#     print("No")
-
 n = int(input())
 s = [input() for _ in range(n)]
 s1 = "HDCS"
@@ -20,7 +5,6 @@ s2 = "A23456789TJQK"
 ans = True
 for i in range(n):
 	for j in range(i):
-		print(i, j)
 		if s[i] == s[j]:
 			ans = False
 for i in range(n):
