7cb4c7cd7dc43ba028b65c779c61a8d74e9d7a31
hirozo99 <hiro.1999@me.com>
2022-11-18 13:27:59 +0900

    add

--- ABC276/A.py
+++ ABC276/A.py
@@ -23,5 +23,5 @@ def main():
             return
     print(-1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ ABC276/B.py
@@ -0,0 +1,3 @@
+n, m = map(int, input().split())
+a = [[] for _ in range(n)]
+
