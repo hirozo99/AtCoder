41b735baddb4c9a1804a57a7157dbcfb4009d390
hirozo99 <hiro.1999@me.com>
2022-11-19 15:54:31 +0900

    add

--- ABC276/B.py
+++ ABC276/B.py
@@ -1,3 +1,26 @@
 n, m = map(int, input().split())
-a = [[] for _ in range(n)]
+graph = [[] for _ in range(n)]
 
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a-1].append(b)
+    graph[b-1].append(a)
+
+for (i, j) in enumerate(graph):
+    j.sort()
+    print(len(j), *j)
+
+# n, m = map(int, input().split())
+# a = [ None ] * m
+# b = [ None ] * m
+# for i in range(m):
+#     a[i], b[i] = map(int, input().split())
+#
+# g = [ [] for i in range(n+1)]
+# for i in range(m):
+#     g[a[i]].append(b[i])
+#     g[b[i]].append(a[i])
+#
+# for i, j in enumerate(g):
+#     j.sort()
+#     print(len(j), *j)
