eeed2737c1d8b92af4301514c3bc95941c534c73
hirozo99 <hiro.1999@me.com>
2022-12-02 20:43:27 +0900

    add

---
+++ ABC/ABC255/A.py
@@ -0,0 +1,13 @@
+R, C = map(int, input().split())
+A11, A12 = map(int, input().split())
+A21, A22 = map(int, input().split())
+
+if R == 1 and C == 1:
+    print(A11)
+elif R == 1 and C == 2:
+    print(A12)
+elif R == 2 and C == 1:
+    print(A21)
+elif R == 2 and C == 2:
+    print(A22)
+
