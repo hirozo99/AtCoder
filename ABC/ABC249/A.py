659a9223dff353d25323c6bb15b09b465ccc370f
hirozo99 <hiro.1999@me.com>
2022-12-06 16:24:00 +0900

    add

--- ABC/ABC249/A.py
+++ ABC/ABC249/A.py
@@ -1,2 +1,16 @@
-N = int(input())
+def solve(A, B, C, X):
+    q = X // (A + C)
+    r = X % (A + C)
+    return (q * A + min(A, r)) * B
 
+A, B, C, D, E, F, X = map(int, input().split())
+
+takahashi = solve(A, B, C, X)
+aoki = solve(D, E, F, X)
+
+if takahashi > aoki:
+    print("Takahashi")
+elif takahashi < aoki:
+    print("Aoki")
+else:
+    print("Draw")
--- cheat_sheet.py
+++ cheat_sheet.py
@@ -2,4 +2,3 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 a = [int(input()) for _ in range(n)]
-
