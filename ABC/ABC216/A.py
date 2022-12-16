08aea9ec4cb07b6355ade7aa21b1d4235eea509d
katohiro <hiro.1999@me.com>
2022-12-16 22:06:33 +0900

    add

--- ABC/ABC216/A.py
+++ ABC/ABC216/A.py
@@ -1,9 +1,9 @@
-x, y = map(int, input().split())
+x, y = map(int, input().split('.'))
 
 if 0 <= y <= 2:
-    print("x" + "-")
+    print(str(x) + "-")
 elif 3 <= y <= 6:
-    print("x")
+    print(x)
 else:
-    print("x" + "+")
+    print(str(x) + "+")
     
