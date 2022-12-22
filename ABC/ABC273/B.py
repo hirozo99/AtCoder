384406d7483bb5c9b7e40853dcd5a867a38c94cb
katohiro <hiro.1999@me.com>
2022-12-22 10:38:32 +0900

    add

---
+++ ABC/ABC273/B.py
@@ -0,0 +1,33 @@
+X, K = map(int, input().split())
+
+for i in range(1, K + 1):
+    target = X % 10 ** i
+    if target >= 5 * 10 ** (i - 1):
+        X += 10 ** i - target
+    else:
+        X -= target
+
+print(X)
+
+x, k = map(int, input().split())
+for i in range(k + 1):
+    x = round(x + 0.5, -i)
+
+print(int(x))
+
+#解説の実装例
+X, K = input().split()
+X = list(map(int, list("0" + X))) # Xを桁ごとに分けてリストにする
+K = int(K)
+N = len(X)
+if K >= N: print(0)# 自明に0になる答え
+else:
+  for i in reversed(range(N - K, N)):
+    if X[i] >= 5: X[i - 1] += 1 # 繰り上がり
+    X[i] = 0 # 四捨五入のどちらであってもこの桁は0になる
+  for i in reversed(range(1, N - K)):
+    if X[i] >= 10: X[i - 1] += 1 # 繰り上がり
+    X[i] %= 10 # この桁に入るのは勿論一桁
+  for start in range(N): # 先頭の0は除外する
+    if X[start] != 0: break
+  for i in range(start, N): print(X[i], end="")
