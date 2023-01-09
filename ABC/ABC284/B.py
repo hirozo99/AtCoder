bcbc599728d9cd717764c7b445af29bd8061fa12
katohiro <hiro.1999@me.com>
2023-01-09 18:52:51 +0900

    add

--- ABC/ABC284/B.py
+++ ABC/ABC284/B.py
@@ -1,17 +1,10 @@
 T = int(input())
 
-s = []
-def is_odd():
-    ans = 0
+for i in range(T):
     N = int(input())
     A = list(map(int, input().split()))
-    for i in range(N):
-        if A[i] % 2 != 0:
+    ans = 0
+    for j in A:
+        if j % 2 == 1:
             ans += 1
-    s.append(ans)
-
-for t in range(T):
-    is_odd()
-
-for i in range(T):
-    print(s[i])
+    print(ans)
