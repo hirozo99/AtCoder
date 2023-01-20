382211651ccb503770840a4e22ce28ae394395a4
katohiro <hiro.1999@me.com>
2023-01-20 16:29:38 +0900

    add

--- ABC/ABC257/B.py
+++ ABC/ABC257/B.py
@@ -37,3 +37,18 @@ for i in range(len(box)):
         ans.append(i)
 
 print(*ans)
+
+# 解説の実装例
+# n,k,q = map(int, input().split())
+# a = list(map(int,input().split()))
+# l= list(map(int,input().split()))
+#
+# for i in range(q):
+#     if(a[l[i]-1] == n):
+#         continue
+#     elif(l[i]==k):
+#         a[l[i]-1]+=1
+#     elif(a[l[i]-1]+1<a[l[i]]):
+#         a[l[i]-1]+=1
+#
+# print(*a)
