d01c21f71f162d4bbe821685bd37ad6cfe98d63e
katohiro <hiro.1999@me.com>
2023-01-22 22:07:50 +0900

    add

--- ABC/ABC286/B.py
+++ ABC/ABC286/B.py
@@ -1,13 +1,4 @@
 N = int(input())
-S = list(input())
+S = input()
 
-ans = []
-for i in range(N):
-    if S[i] == "n" and i < N-1:
-        if S[i+1] == "a":
-            ans.append(S[i]+"y")
-        else:
-            ans.append(S[i])
-    else:
-        ans.append(S[i])
-print("".join(ans))
+print(S.replace("na", "nya"))
