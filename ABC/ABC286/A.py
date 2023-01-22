be95107701e186d70a3023daaa6e04ceb3fb956e
katohiro <hiro.1999@me.com>
2023-01-22 21:58:23 +0900

    add

--- ABC/ABC286/A.py
+++ ABC/ABC286/A.py
@@ -1,18 +1,5 @@
 N, P, Q, R, S = map(int, input().split())
 A = list(map(int, input().split()))
 
-f = []
-s = []
-
-for i in range(P, Q+1):
-    f.append(A[i-1])
-
-for i in range(R, S+1):
-    s.append(A[i-1])
-
-A[P-1:Q] = s
-A[R-1:S] = f
+A[P-1:Q], A[R-1:S] = A[R-1:S], A[P-1:Q]
 print(*A)
-
-# print(*f)
-# print(*s)
