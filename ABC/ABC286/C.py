af6692ea734faf4da7d37f9293f8f9f945ba024c
katohiro <hiro.1999@me.com>
2023-01-27 20:49:18 +0900

    add

--- ABC/ABC286/C.py
+++ ABC/ABC286/C.py
@@ -1,12 +1,13 @@
-N, A, B = map(int, input().split())
-S = input()
-ans = 0
+n, a, b = map(int, input().split())
+s = input()
 
+s += s
+ans = 1 << 60
+for i in range(n):
+    tmp = a * i
+    for j in range(n // 2):
+        if s[i + j] != s[i + n - 1 - j]:
+            tmp += b
+    ans = min(ans, tmp)
+print(ans)
 
-if not S[:N] == S[0:]:
-    if len(S) > 1:
-        S = S[1:] +S[0]
-        ans += A
-
-else:
-    print(0)
