27e57c22493440f9a82ed26ec6a1f7d359dcec17
katohiro <hiro.1999@me.com>
2023-02-06 16:54:37 +0900

    add

--- ABC/ABC288/C.py
+++ ABC/ABC288/C.py
@@ -1,46 +1,24 @@
-n, m = map(int, input().split())
+N,M = map(int, input().split())
+Adj = [[] * N for _ in range(N)]
 
-A_list = []
-B_list = []
-for i in range(m):
-    A, B = map(int, input().split())
-    A_list.append(A)
-    B_list.append(B)
+for _ in range(M):
+  A,B = map(int, input().split())
+  Adj[A-1].append(B-1)
+  Adj[B-1].append(A-1)
 
-print(A_list, B_list)
-pair = [[A_list[i], B_list[i]] for i in range(m)]
-print(pair)
-pair.sort()
-print(pair)
+def dfs(x):
+  for y in Adj[x]:
+    if not visited[y]:
+      visited[y] = True
+      dfs(y)
 
+visited = [False] * N
+cyc = 0
 
-# 頂点数:n
-# edge[i]:頂点iと辺で結ばれる頂点の集合
-# 遷移元の頂点
-parent=[-1]*n
-# 閉路に含まれる辺の集合
-loop =set()
-# 既に探索した頂点か
-come=[False]*n
+for i in range(N):
+  if not visited[i]:
+    visited[i] = True
+    dfs(i)
+    cyc += 1
 
-def dfs(x,last=-1):
-    if last!=-1:
-        parent[x]=last
-    come[x]=True
-    for i in edge[x]:
-        if i ==last:continue
-        if come[i]:
-            now=x
-            loop.add((now,i))
-            loop.add((i,now))
-            while now!=i:
-                loop.add((now,parent[now]))
-                loop.add((parent[now],now))
-                now=parent[now]
-            return True
-        else:
-            if dfs(i,x):
-                return True
-    return False
-
-dfs(0)
+print(M - N + cyc)
