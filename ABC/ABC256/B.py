8c54c632b48fe5290fd49149ca69cb971ec64557
katohiro <hiro.1999@me.com>
2023-02-09 23:44:40 +0900

    add

--- ABC/ABC256/B.py
+++ ABC/ABC256/B.py
@@ -1,11 +1,32 @@
-N = int(input())
-A = list(map(int, input().split()))
+# 入力の受け取り
+N=int(input())
+A=list(map(int,input().split()))
 
-P = 0
-base = [0]*4
+# Pに初期値0を入れる
+P=0
 
+# 駒があるマス目
+# 最初はマス0に1個
+Koma=[0]
+
+# i=0~(N-1)
 for i in range(N):
-    base[0] = 1
-    for j in range(4):
-        if base[j] == 1:
-            base[j] = base[A[i]]
+    # 新しい駒の場所
+    NewKoma=[0]
+
+    # Komaの値を順にxへ入力して処理
+    for x in Koma:
+        # (x+A[i])<4⇔マス目の範囲内なら
+        if x+A[i]<4:
+            # 新しいマス目を記録
+            NewKoma.append(x+A[i])
+        # そうでないなら(マス目の範囲を超えたら)
+        else:
+            # Pにプラス1
+            P+=1
+
+    # KomaをNewKomaで更新
+    Koma=NewKoma
+
+# 答えの出力
+print(P)
