602c7e3d1f04a11236dc5112a24f342c15cdc78b
katohiro <hiro.1999@me.com>
2023-02-20 22:01:12 +0900

    add

--- ABC/ABC290/C.py
+++ ABC/ABC290/C.py
@@ -1,26 +1,14 @@
-from itertools import combinations
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-def mex(nums):
-    smallest = 0
-    nums_set = set(nums)
-
-    while True:
-        if smallest not in nums_set:
-            return smallest
-        smallest += 1
-
-def subsequence(a, k):
-    subsequences = []
-    for comb in combinations(a, k):
-        subsequences.append(list(comb))
-    return subsequences
-
-check = subsequence(A, K)
-ans = 0
-
-for i in range(len(check)):
-    if max(mex(check[i]), ans) != ans:
-        ans = max(mex(check[i]), ans)
-print(ans)
+a = list(set(A))
+# print(a)
+a.sort()
+c = 0
+for i in a:
+    if c == i:
+        c += 1
+    else:
+        break
+# print(c)
+print(min(K, c))
