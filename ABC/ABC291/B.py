6021f41fc83a13e9270e17b5b5e304de8aa042eb
katohiro <hiro.1999@me.com>
2023-03-01 01:27:58 +0900

    add

--- ABC/ABC291/B.py
+++ ABC/ABC291/B.py
@@ -1,20 +1,20 @@
-N = int(input())
-X = list(map(int, input().split()))
+# N = int(input())
+# X = list(map(int, input().split()))
 
-X.sort()
-for i in range(N):
-    X.pop(0)
-    X.pop(-1)
+# X.sort()
+# for i in range(N):
+#     X.pop(0)
+#     X.pop(-1)
 
 
-cnt = 0
-for i in X:
-    cnt += i
+# cnt = 0
+# for i in X:
+#     cnt += i
 
-ans = cnt / len(X)
-print(f'{ans:.15f}')
+# ans = cnt / len(X)
+# print(f'{ans:.15f}')
 
-"""
+
 # 短い実装方法
 N = int(input())
 X = list(map(int, input().split()))
@@ -24,4 +24,3 @@ x = X[N:-N]
 
 # print(x)
 print(sum(x)/(3*N))
-"""
