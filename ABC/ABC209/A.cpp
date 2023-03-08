9d8138d808f2a2ca444977bd9da10b7d9a49606d
katohiro <hiro.1999@me.com>
2023-03-08 21:35:00 +0900

    add

---
+++ ABC/ABC209/A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << max(0, B-A+1) << endl;
+    return 0;
+}
---
+++ ABC/ABC253/B.py
@@ -0,0 +1,4 @@
+H, W = map(int, input().split())
+S = [[input()] for i in range(H)]
+
+print(S)
