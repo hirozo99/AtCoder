db50b25d06ba1f503cff2e38172574683035fad9
katohiro <hiro.1999@me.com>
2023-03-19 18:19:48 +0900

    add

--- ABC/ABC200/C.py
+++ ABC/ABC200/C.py
@@ -1,3 +1,12 @@
 N = int(input())
+A = list(map(int, input().split()))
 
-print((N-1)//100+1)
+R = [x % 200 for x in A]
+cnt = [0] * 200
+
+ans = 0
+for r in R:
+    ans += cnt[r]
+    cnt[r] += 1
+
+print(ans)
