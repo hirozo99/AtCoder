ccbb13d58fc72aa85d6413482b1c1660d12c6fdf
katohiro <hiro.1999@me.com>
2023-03-29 23:32:59 +0900

    add

---
+++ ABC/ABC294/C.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+C = sorted(A + B)
+
+get_index = {c: i + 1 for i, c in enumerate(C)}
+
+print(get_index)
+for a in A:
+    print(get_index[a])
+
+for b in B:
+    print(get_index[b])
